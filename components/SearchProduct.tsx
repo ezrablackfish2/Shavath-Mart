839ea71865cdea6c17a95b6bc22100190ed0a15d
ezra <ezrayeneneh1992@gmail.com>
2023-10-12 09:01:31 +0000

    search button product not link in max width

--- components/SearchProduct.tsx
+++ components/SearchProduct.tsx
@@ -5,7 +5,7 @@ export default function SearchProduct() {
     return (
         <div className={styles.Search}>
             <input className={styles.searchbar} type="text" placeholder="Search Products"/>
-	<a className={styles.searchbuttonlink} href="/search">
+	<a className={styles.searchbuttonlink}>
             <Button className={styles.searchbutton} text="Search" image="/search.png" imageclass={styles.searchimage}/>
 	</a>
         </div>
