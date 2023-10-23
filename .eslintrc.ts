eb42a779ae9d0a632ae98faad6d6d6149272ed82
ezra <ezrayeneneh1992@gmail.com>
2023-10-23 08:18:36 +0000

    eslint

--- .eslintrc.ts
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // ...other ESLint configuration options...
-
-  // Add this to disable the specific rule that's causing the warning
-  rules: {
-    'next/no-img-element': 'off', // Disable the specific rule
-  },
-};
-
