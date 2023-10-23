69aa691144c19b878f0580c83f1fba499d331387
ezra <ezrayeneneh1992@gmail.com>
2023-10-23 08:29:52 +0000

    ts 4

--- components/Search.tsx
+++ components/Search.tsx
@@ -13,7 +13,6 @@ export default function Search( {search, setSearch}: Props ) {
     return (
         <div className={styles.Search}>
             <input className={styles.searchbar} type="text" placeholder="Search" onChange={handleSearch}/>
-	{console.log(search)}
 		<span className={styles.searchbuttonlink}>
             <Button className={styles.searchbutton} text="Search" image="/search.png" imageclass={styles.searchimage} width="" bgColor=""/>
 		</span>
